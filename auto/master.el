550289c5d4df9434741700a61e5d7633a5052f38
Caio De Naday Hornhardt <caiodnh@gmail.com>
2012-09-10 16:16:55 -0300

    master e primeiro chap, secos

---
+++ auto/master.el
@@ -0,0 +1,12 @@
+(TeX-add-style-hook "master"
+ (lambda ()
+    (TeX-run-style-hooks
+     "dissertacao"
+     "senosstyle"
+     "latex2e"
+     "bk11"
+     "book"
+     "a4paper"
+     "11pt"
+     "geometria_algebrica_por_funcoes")))
+
